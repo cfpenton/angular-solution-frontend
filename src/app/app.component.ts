26f8b61b4d99cadc3758b10bbdb498c7963167d5
cfpenton <cfpenton@gmx.com>
2022-07-15 17:23:35 -0400

    init-setting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  logo: string ='../assets/images/agence-logo.png';
   title = 'angular-solution-frontend';
 }
